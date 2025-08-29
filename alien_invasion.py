f96b035eff24bc0306c4e83484e50e5619e4e368
bithcc <bithcc@163.com>
2025-08-29 13:14:34 +0800

    实现了子弹与外星人的碰撞检测功能

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,7 +27,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
-            print(len(self.bullets))
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
 
@@ -67,6 +67,20 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        
+        self._check_bullet_alien_collisions()
+        
+    def _check_bullet_alien_collisions(self):
+        collisions = pygame.sprite.groupcollide(
+            self.bullets,self.aliens,True,True)
+        
+        if not self.aliens:
+            self.bullets.empty()
+            self._create_fleet()
+            
+    def _update_aliens(self):
+        self._check_fleet_edges()
+        self.aliens.update()
 
     def _create_alien(self, x_position, y_position):
         new_alien = Alien(self)
@@ -74,7 +88,18 @@ class AlienInvasion:
         new_alien.rect.x = x_position
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
-        
+
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
     def _create_fleet(self):
         alien = Alien(self)
         alien_width, alien_height = alien.rect.size
